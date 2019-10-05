ed65efb1d5dfd41ec167aa49c161de9fc20fed03
Unsmoker <hasangaly@gmail.com>
2019-10-06 00:07:03 +0600

    add Task countUniqueValues

---
+++ MultyPointerPattern/CountUniqueValues/Readme.md
@@ -0,0 +1,2 @@
+# Find The Count of Unique Values
+Write the function wich will get input array and will return count of unique values
---
+++ MultyPointerPattern/CountUniqueValues/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func countUniqueValue(a []int) int {
+	var i int
+	i = 0
+	lena := len(a)
+	for j := 1; j < lena; j++ {
+		if a[i] != a[j] {
+			i++
+			a[i] = a[j]
+		}
+	}
+	return i + 1
+}
+func main() {
+	fmt.Println(countUniqueValue([]int{1, 1, 2, 3, 4, 5, 5, 5, 5, 6, 7}))
+}
